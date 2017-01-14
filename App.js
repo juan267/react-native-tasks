64d7f1ee7c0a7bd8ef9f91cb2a71c5e25f301f23
juan267 <j.gomez267@gmail.com>
2017-01-14 15:18:12 -0500

    Basic layout

---
+++ App.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Platform,
+} from 'react-native';
+import Header from './Header'
+import Footer from './Footer'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+
+        </View>
+        <Footer />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    ...Platform.select({
+      ios: { paddingTop: 30}
+    })
+  },
+  content: {
+    flex: 1
+  }
+});
+
+
+export default App;
